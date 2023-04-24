d2acd2542495e82b55ff80b048248f8ba976057b
VEEETOOORRR <antonni2601.ag@gmail.com>
2023-04-24 10:27:16 -0300

    ultrassom v1

--- Em Testes/ultrassom e reflexao.py
+++ Em Testes/ultrassom e reflexao.py
@@ -3,7 +3,7 @@
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Port
-from time import sleep
+from pybricks.tools import wait
 
 ev3 = EV3Brick()
 
@@ -13,35 +13,54 @@ right_motor = Motor(Port.C)
 corEsquerdo = ColorSensor(Port.S3)
 corDireito = ColorSensor(Port.S2)
 
-distanciaEsquerdo = UltrasonicSensor(Port.S1)
-distanciaDireito = UltrasonicSensor(Port.S4)
-
-velocidadeEsquerdo = 200
-velocidadeDireito = 200
+ultraFrente = UltrasonicSensor(Port.S1)
+ultraLado = UltrasonicSensor(Port.S4)
  
-while True:
-
-    distancia1 = int(ultrassonico1.distance())
-    distancia2 = int(ultrassonico2.distance())
-
-    right_motor.run(velocidadeDireito)
-    left_motor.run(velocidadeEsquerdo)
-
-    valorEsquerdo = (corEsquerdo.reflection())
-    valorDireito = (corDireito.reflection())
-
-    velocidadeEsquerdo = valorEsquerdo * 2
-    velocidadeDireito = valorDireito * 2
-
-    sleep(0.00001)
+podecontinuar = True
 
 while True:
 
-    if distancia1 < 80:
-        ev3.light.on(Color.RED)
+    distancia1 = int(ultraFrente.distance())
+    distancia2 = int(ultraLado.distance())
 
-    else:
-        ev3.light.on(Color.GREEN)
+    if distancia1 > 50 and podecontinuar == True:
 
-    wait(1000)
-    print(distancia1)
+        right_motor.run(velocidadeDireito)
+        left_motor.run(velocidadeEsquerdo)
+
+        valorEsquerdo = (corEsquerdo.reflection())
+        valorDireito = (corDireito.reflection())
+
+        velocidadeEsquerdo = valorEsquerdo * 2
+        velocidadeDireito = valorDireito * 2
+
+    elif distancia1 <= 50 and podecontinuar == True:
+        
+        podecontinuar = False
+        right_motor.stop()
+        left_motor.stop()
+
+    elif podecontinuar == False:
+        right_motor.reset_angle(0)
+        left_motor.reset_angle(0)
+
+        while distancia2 > 50:
+            right_motor.run(100)
+            left_motor.run(-100)
+
+            angulodireito = right_motor.angle()
+            anguloesquerdo = left_motor.angle()
+
+        right_motor.reset_angle(0)
+        left_motor.reset_angle(0)
+
+        while distancia2 < 100:
+            right_motor.run(100)
+            left_motor.run(100)
+
+            percursodireito = right_motor.angle()
+            percursoesquerdo = left_motor.angle()
+            
+
+
+    wait(10)
