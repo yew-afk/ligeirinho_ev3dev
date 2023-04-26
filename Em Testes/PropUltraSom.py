2d9b63663443d6233d7bf3167d579a75dfbbb0cc
VEEETOOORRR <antonni2601.ag@gmail.com>
2023-04-26 15:34:39 -0300

    sa

--- Em Testes/PropUltraSom.py
+++ Em Testes/PropUltraSom.py
@@ -6,6 +6,7 @@ from pybricks.parameters import Port
 from pybricks.tools import wait
 
 valor1 = 250
+valor2 = 50
 
 a = (valor1 - valor2)/73
 b = valor1 - (76 * a)
@@ -16,7 +17,7 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 ultraF = UltrasonicSensor(Port.S1)
-ultraD = UltrasonicSensor(Port.S2)
+ultraD = UltrasonicSensor(Port.S4)
 
 cor_dir = ColorSensor(Port.S2)
 cor_esq = ColorSensor(Port.S3)
@@ -25,13 +26,35 @@ PC = True
 
 while True:
 
-    distF 
+    distF = int(ultraF.distance())
+    distD = int(ultraD.distance())
 
-    valorEsquerdo = (cor_esq.reflection())
-    valorDireito = (cor_dir.reflection())
+    if distF >= 80:
+        
+        valorEsquerdo = (cor_esq.reflection())
+        valorDireito = (cor_dir.reflection())
 
-    vel_direito = a*valorDireito + b
-    vel_esquerdo = a*valorEsquerdo + b
+        vel_direito = a*valorDireito + b
+        vel_esquerdo = a*valorEsquerdo + b
 
-    right_motor.run(vel_direito)
-    left_motor.run(vel_esquerdo)
+        right_motor.run(vel_direito+40)
+        left_motor.run(vel_esquerdo)
+
+        wait(10)
+
+    elif distF < 80:
+
+        left_motor.run(-100)
+        right_motor.run(100)
+        wait(100)
+
+        if distD < 100:
+
+            left_motor.run(150)
+            right_motor.run(150)
+        
+        else:
+
+            left_motor.run(100)
+            right_motor.run(-100)
+            wait(100)
